e80716c7d1c2ca1e5a8fac056736f97e585b7df5
edvu <edvino.ur@gmail.com>
2021-04-28 21:44:40 +0300

    Set2a 11

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -170,4 +170,7 @@ eitherDiv x y | y == 0 = Left (show x ++ "/" ++ show  y)
 --   addEithers (Left "boom") (Left "fail") ==> Left "boom"
 
 addEithers :: Either String Int -> Either String Int -> Either String Int
-addEithers a b = todo
+addEithers (Left a) (Left b) = Left a
+addEithers (Right a) (Left b) = Left b
+addEithers (Left a) (Right b) = Left a
+addEithers (Right a) (Right b) = (Right (a + b))
